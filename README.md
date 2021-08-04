dcfe1fe8652ab1ada95f8ca8a1a3d7b59074ec03
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-08-04 18:02:17 -0400

    displaying plain html

---
+++ README.md
@@ -0,0 +1,1 @@
+# Little Node Browser
--- src/index.js
+++ src/index.js
@@ -1,30 +1,89 @@
-import { QMainWindow, QLabel, Qwidget } from "@nodegui/nodegui";
+import { 
+    QMainWindow, 
+    QLabel, 
+    QWidget, 
+    FlexLayout, 
+    QPushButton, 
+    QLineEdit, 
+    QPlainTextEdit } from "@nodegui/nodegui";
 import axios from "axios";
 
 async function fetchWebPage(url) {
-    //const { data } = await axios.get("http://www.example.com/")
-console.log('other function')
-    //console.log(data)
-    //return data;
-    // const webPage = new QLabel();
-    // webPage.setText(data)
-    // return webPage;
+    const { data } = await axios.get(url)
+    console.log(data)
+    return data;
 }
-
-fetchWebPage()
 
 async function main() {
     const win = new QMainWindow();
     win.setWindowTitle("Little Node Browser");
-console.log('main function')
+    win.resize(600, 600);
 
-    const { data } = await axios.get("http://www.example.com/")
-    console.log(typeof data)
+    const centralWidget = new QWidget(); 
+    const centralLayout = new FlexLayout();
+    centralWidget.setObjectName("myroot");
+    centralWidget.setLayout(centralLayout);
+
+    //const { data } = await axios.get("http://www.example.com/")
+    //console.log(typeof data)
+// ---------------------------------- URL BAR ----------------------------------
+    const urlBar = new QWidget();
+    const urlBarLayout = new FlexLayout();
+    urlBar.setObjectName("urlBar");
+    urlBar.setLayout(urlBarLayout);
+
     const label = new QLabel();
-    //const webPage = await fetchWebPage();
-    label.setText(data);
+    label.setObjectName("addressBar");
+    label.setText("Address");
 
-    win.setCentralWidget(label);
+    const inputBox = new QLineEdit();
+    inputBox.setObjectName("inputBox");
+
+    const searchButton = new QPushButton();
+    searchButton.setObjectName("searchBtn");
+    searchButton.setText("Search");
+
+    urlBarLayout.addWidget(label);
+    urlBarLayout.addWidget(inputBox);
+    urlBarLayout.addWidget(searchButton);
+
+// ------------------------------ PAGE DISPLAY -------------------------------
+    const pageDisplay = new QPlainTextEdit();
+    pageDisplay.setObjectName("pageDisplay");
+    pageDisplay.setReadOnly(true);
+
+// ------------------------------- FUNCTIONALITY -----------------------------
+    searchButton.addEventListener('clicked', async () => {
+        const enteredUrl = inputBox.text();
+        const response = await fetchWebPage(enteredUrl)
+        console.log(response)
+
+        pageDisplay.setPlainText(response);
+    })
+
+// ----------------------------- MAIN WINDOW ----------------------------------
+    centralLayout.addWidget(urlBar);
+    centralLayout.addWidget(pageDisplay)
+    
+    const rootStyleSheet = `
+        #myroot {
+            height: 100%;
+        }
+        #urlBar {
+            flex-direction: row;
+            justify-content: space-between;
+            padding: 10px;
+        }
+        #inputBox {
+            width: 300px;
+        }
+        #pageDisplay {
+            height: 500px;
+        }
+    `;
+    centralWidget.setStyleSheet(rootStyleSheet);
+
+    win.setCentralWidget(centralWidget);
     win.show();
 
     global.win = win;
